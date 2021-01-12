e8171185a25adb8ef8a8f147205f91ec339dfedd
James McDermott <james.mcdermott7@mail.dcu.ie>
2021-01-12 00:44:23 +0000

    Update nix-setup.sh

--- admin-scripts/nix-setup.sh
+++ admin-scripts/nix-setup.sh
@@ -1,11 +1,17 @@
-curl -s https://gist.githubusercontent.com/theycallmemac/19ac8930570f2d88b645c3d5eab10169/raw/f726cb7c0d1f2dd3c87b89c2a131de845c38e799/configuration.nix > /etc/nixos/configuration.nix
+### copy default config file with git to clone repository
+curl -s https://gist.githubusercontent.com/theycallmemac/19ac8930570f2d88b645c3d5eab10169/raw/286b1bc05279c056d55a331a92970101132fbc69/configuration.nix > /etc/nixos/configuration.nix
 nixos-rebuild switch
+
+### clone repository and copy contents to nixos directory
 git clone https://github.com/theycallmemac/nix-configs /etc/nixos2
 pushd /etc/nixos2
-git checkout -b ec2 remotes/origin/ec2
 cp -R /etc/nixos2/* /etc/nixos/
 cp -R /etc/nixos2/.git /etc/nixos/.git
+
+### add home manager channel
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-20.09.tar.gz home-manager
 nix-channel --update
 nixos-rebuild switch
+
+### remove original cloned directory
 rm -r /etc/nixos2
